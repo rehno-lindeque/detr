3969a3e65eda60dd8d8dd3f6316771d1e8b7b780
Rehno Lindeque <errantkid@gmail.com>
2020-11-12 12:44:14 -0500

    remove problematic metric logging (divide by 0 issue)

--- util/misc.py
+++ util/misc.py
@@ -245,8 +245,11 @@ class MetricLogger(object):
             end = time.time()
         total_time = time.time() - start_time
         total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-        print('{} Total time: {} ({:.4f} s / it)'.format(
-            header, total_time_str, total_time / len(iterable)))
+        if len(iterable) > 0:
+            print('{} Total time: {} ({:.4f} s / it)'.format(
+                header, total_time_str, total_time / len(iterable)))
+        else:
+            print('{} Total time: {} (len(iterable) = 0)'.format(header, total_time_str))
 
 
 def get_sha():
